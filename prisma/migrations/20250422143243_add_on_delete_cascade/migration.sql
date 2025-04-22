6579abde7e9a31c8211cd178ff9898a0ef6c5c00
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2025-04-22 23:22:28 +0700

    fix-migration 2

--- prisma/migrations/20250422143243_add_on_delete_cascade/migration.sql
+++ prisma/migrations/20250422143243_add_on_delete_cascade/migration.sql
@@ -5,10 +5,10 @@ ALTER TABLE `history` DROP FOREIGN KEY `History_liveId_fkey`;
 ALTER TABLE `Live` DROP FOREIGN KEY `Live_videoId_fkey`;
 
 -- DropIndex
-DROP INDEX `History_liveId_fkey` ON `history`;
+DROP INDEX `History_liveId_fkey` ON `History`;
 
 -- DropIndex
-DROP INDEX `Live_videoId_fkey` ON `live`;
+DROP INDEX `Live_videoId_fkey` ON `Live`;
 
 -- AddForeignKey
 ALTER TABLE `Live` ADD CONSTRAINT `Live_videoId_fkey` FOREIGN KEY (`videoId`) REFERENCES `Video`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
